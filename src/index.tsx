6fbf847cd6e3103b645bf9203ba94ff827d98924
al3xdiaz <alexleonel96@hotmail.com>
2023-11-05 16:44:08 -0600

    [fix] babel files

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,13 @@ import form from "@/src/form";
 import {TextArea,TextField} from "@/src/form/input";
 import {PasswordField} from "@/src/form/password";
 import {Button} from "@/src/form/btn";
-import { IForm } from "@/types";
 
+export interface IForm extends React.FC<any>{
+    TextField:React.FC<any> ;
+    TextArea:React.FC<any> ;
+    PasswordField:React.FC<any> ;
+    Button:React.FC<any> ;
+}
 
 export const Form:IForm = Object.assign(form,{
     TextField,
--- types/index.ts
+++
@@ -1,6 +0,0 @@
-export interface IForm extends React.FC<any>{
-    TextField:React.FC<any> ;
-    TextArea:React.FC<any> ;
-    PasswordField:React.FC<any> ;
-    Button:React.FC<any> ;
-}
