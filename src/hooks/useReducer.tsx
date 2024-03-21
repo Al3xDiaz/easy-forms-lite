3276bed086b6d263bd5bae21e497199c7266ebb8
al3xdiaz <alexleonel96@hotmail.com>
2024-03-20 18:47:45 -0600

    [fix]  set state

--- src/hooks/useReducer.tsx
+++ src/hooks/useReducer.tsx
@@ -2,13 +2,12 @@ import { IAction } from "../context/reducer";
 import { IState } from "../context/context";
 
 export const reducer = (state: IState, action:IAction ):IState => {
-    console.log(action.type)
     switch (action.type) {
         case "SET_PROP":
             return {
                 ...state,
                 data:{
-                    ...state,
+                    ...state.data,
                     [action.payload.name] : action.payload.value,
                 }
             };
@@ -18,12 +17,10 @@ export const reducer = (state: IState, action:IAction ):IState => {
                 formData:new FormData(),
             }
         case "SET_PROP_FORMDATA":
-            console.log("before:",state)
             state.formData.delete(action.payload.name)
             Array.from(action.payload.value).forEach(file=>{
                 state.formData.append(action.payload.name,file)
             })
-            console.log("after:",state)
             return state;
         default:
             return state;
