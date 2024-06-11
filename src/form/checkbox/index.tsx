d4f2a66645d5eab6de611d9509a1de62f3520ca1
al3xdiaz <alexleonel96@hotmail.com>
2024-06-10 22:15:24 -0600

    [patch]  checkbox

--- src/form/checkbox/index.tsx
+++ src/form/checkbox/index.tsx
@@ -14,9 +14,10 @@ export const CheckBox:React.FC<Iprops> =(props)=>{
     } = props;
     const {value,setProp} = useForm<boolean>(name);
     return (
-        <Label {...extraProps} $name={name}>
+        <Label {...extraProps} $name={name} style={{outline:"none",padding:".8rem"}}>
             <input checked={value} type="checkbox" onChange={e=>{setProp({name,value:e.target.checked})}}/>
             <div className="checkmark"></div>
+            {!!props.label&&<span>{props.label}</span>}
         </Label>
     )
 }
