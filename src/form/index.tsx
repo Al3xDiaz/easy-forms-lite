2ac3dd1c89044b0c9d30ffd3fc2b238b0aee7331
al3xdiaz <alexleonel96@hotmail.com>
2024-06-20 21:45:44 -0600

    [patch]  styles use stylecomponents

--- src/form/index.tsx
+++ src/form/index.tsx
@@ -1,47 +1,71 @@
 import React, { CSSProperties, FC, useReducer, } from "react";
 import {context,Dictionary,reducer} from "../context";
-import "index.css"
+import styled from "styled-components";
 
 export interface IInput{
-    name: string;
-    label?:string,
-    required?:boolean,
-    placeholder?:string,
-    style?:React.StyleHTMLAttributes<HTMLStyleElement>,
-    className?:string,
+  name: string;
+  label?:string,
+  required?:boolean,
+  placeholder?:string,
+  style?:React.StyleHTMLAttributes<HTMLStyleElement>,
+  className?:string,
 }
 type IPersitData ={persistData?: false;} | {persistData: true}
 
 interface IForm{
-    children?: React.JSX.Element | Array<React.JSX.Element>;
-    onSubmit? : (data:Dictionary,formData:FormData)=>(Promise<void> | void);
-    className?: string;
-    styles?:CSSProperties;
-    initialState?: Dictionary;
+  children?: React.JSX.Element | Array<React.JSX.Element>;
+  onSubmit? : (data:Dictionary,formData:FormData)=>(Promise<void> | void);
+  className?: string;
+  styles?:CSSProperties;
+  initialState?: Dictionary;
 }
 type IFormProps = IForm & IPersitData
 
 const FormState:FC<IFormProps> =  ({initialState={},children,className,onSubmit,persistData,styles})=>{
-    const [state,dispatch]= useReducer(reducer,{data:initialState,formData:new FormData(),status:"ready"});
-    const HandleSubmit = async (e:React.FormEvent<HTMLFormElement>)=>{
-        try {
-            dispatch({type:"SET_STATUS",payload:"loading"})
-            e.preventDefault();
-            onSubmit && await onSubmit(state.data,state.formData);
-            if(!persistData){
-                dispatch && dispatch({type:"SET_EMPTY"});
-            }
-            dispatch({type:"SET_STATUS",payload:"ready"})
-        } catch (error) {
-            dispatch({type:"SET_STATUS",payload:"error"})
-        }
+  const [state,dispatch]= useReducer(reducer,{data:initialState,formData:new FormData(),status:"ready"});
+  const HandleSubmit = async (e:React.FormEvent<HTMLFormElement>)=>{
+    try {
+      dispatch({type:"SET_STATUS",payload:"loading"})
+      e.preventDefault();
+      onSubmit && await onSubmit(state.data,state.formData);
+      if(!persistData){
+        dispatch && dispatch({type:"SET_EMPTY"});
+      }
+      dispatch({type:"SET_STATUS",payload:"ready"})
+    } catch (error) {
+      dispatch({type:"SET_STATUS",payload:"error"})
     }
-    return (
-        <context.Provider value={{state,dispatch}}>
-            <form aria-label="form" className={`${className || ""} ${state.status}`} style={styles} onSubmit={HandleSubmit}>
-                <>{children}</>
-            </form>
-       </context.Provider>
-    )
+  }
+  return (
+    <context.Provider value={{state,dispatch}}>
+      <Form aria-label="form" className={`${className || ""} ${state.status}`} style={styles} onSubmit={HandleSubmit}>
+        <>{children}</>
+      </Form>
+    </context.Provider>
+  )
 }
+
+const Form = styled.form`
+  & button:disabled {
+    background-color: rgb(90, 90, 90);
+  }
+  & button:disabled::after {
+    background-color: rgb(121, 121, 121);
+  }
+  &.ready label button .loading {
+    display: none;
+  }
+  &.loading button span.icon-right {
+    position: absolute;
+    top: .5rem;
+    right: .1rem;
+    width: 2rem;
+    height: 2rem;
+  }
+  & {
+    display: grid;
+    grid-template-areas: "mames mames" "send ." ;
+    gap: 1rem;
+  }
+`;
 export default FormState;
